4eafd1e0dfe6f319bda36d76e27f2ca5d8fb46b5
BrotherlyHamlet16 <80749000+Clever-Niwagaba@users.noreply.github.com>
2025-06-12 12:20:42 +0300

    routes definition

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,24 @@
 <?php
 
+use App\Http\Controllers\API\AuthController;
+use App\Http\Controllers\API\InvestmentController;
+use App\Http\Controllers\API\InvestmentPlanController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+// Auuthentication routes
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+
+    Route::get('/plans', [InvestmentPlanController::class, 'index']);
+
+    Route::get('/investments', [InvestmentController::class, 'index']);
+    Route::post('/investments', [InvestmentController::class, 'store']);
+    Route::post('/investments/{id}/withdraw', [InvestmentController::class, 'withdraw']);
 });
