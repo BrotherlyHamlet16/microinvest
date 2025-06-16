b021f3e77f64012454efb16f1e0ab3b0dc179b46
BrotherlyHamlet16 <80749000+Clever-Niwagaba@users.noreply.github.com>
2025-06-16 15:55:12 +0300

    updated models and relationships

--- app/Models/Investment.php
+++ app/Models/Investment.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Investment extends Model
 {
-    //
+    public function plan()
+    {
+        return $this->belongsTo(InvestmentPlan::class);
+    }
 }
