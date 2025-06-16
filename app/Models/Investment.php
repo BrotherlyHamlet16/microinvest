8060f185919a50c590fe690c4afb0ef1dbd0e81c
BrotherlyHamlet16 <80749000+Clever-Niwagaba@users.noreply.github.com>
2025-06-16 16:21:19 +0300

    updated models

--- app/Models/Investment.php
+++ app/Models/Investment.php
@@ -3,11 +3,32 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Investment extends Model
 {
+    use HasFactory;
+    protected $fillable = [
+        'user_id',
+        'plan_id',
+        'amount',
+        'status',
+        'start_date',
+        'end_date',
+    ];
+
+    protected $dates = [
+        'start_date',
+        'end_date',
+        'maturity_date',
+    ];
     public function plan()
     {
         return $this->belongsTo(InvestmentPlan::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
     }
 }
--- app/Models/InvestmentPlan.php
+++ app/Models/InvestmentPlan.php
@@ -2,10 +2,18 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class InvestmentPlan extends Model
 {
+    use HasFactory;
+    protected $fillable = [
+        'name',
+        'daily_return_rate',
+        'lock_period',
+    ];
+
     public function investments()
     {
         return $this->hasMany(Investment::class);
